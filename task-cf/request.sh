7d3ada39d906772fc54b381c22282c2fce4e36ed
Mihai Hernest <hernest001@gmail.com>
2023-01-06 17:20:36 +0200

    error curl added

--- task-cf/request.sh
+++ task-cf/request.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-for i in {1..5}
+for i in {1..10}
 do
   randomNumber=$(($RANDOM % 10))
   timestamp=$(date +%s)
   curl -X POST https://us-central1-task-cf-370908.cloudfunctions.net/task-function -H "Content-Type: application/json" \
   -d "{\"message\":\"message-$i\", \"number_int\":\"$randomNumber\", \"number_float\":\"0.33\", \"timestamp\":\"$timestamp\"}"
+done
+
+for i in {1..3}
+do
+    curl -X POST https://us-central1-task-cf-370908.cloudfunctions.net/task-function -H "Content-Type: application/json" \
+  -d "{\"message\":\"message-$i\", \"number_int\":\"not an int\", \"number_float\":\"0.33\", \"timestamp\":\"timestamp\"}"
 done
