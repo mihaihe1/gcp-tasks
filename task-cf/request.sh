f90f050907355d42002046fcf4cb30b289accc96
Mihai Hernest <hernest001@gmail.com>
2023-01-06 17:03:31 +0200

    request changes

--- task-cf/request.sh
+++ task-cf/request.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-curl -X POST https://us-central1-task-cf-370908.cloudfunctions.net/task-function -H "Content-Type: application/json" \
- -d "{\"demo\":\"test\"}"
+for i in {1..5}
+do
+  randomNumber=$(($RANDOM % 10))
+  timestamp=$(date +%s)
+  curl -X POST https://us-central1-task-cf-370908.cloudfunctions.net/task-function -H "Content-Type: application/json" \
+  -d "{\"message\":\"message-$i\", \"number_int\":\"$randomNumber\", \"number_float\":\"0.33\", \"timestamp\":\"$timestamp\"}"
+done
